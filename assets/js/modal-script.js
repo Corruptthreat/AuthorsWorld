9466ed020d3a97fc5f94d230ae80dd3e9336ad58
Corruptthreat <drawte786@gmail.com>
2021-05-22 18:10:26 +0530

    upto testimonial part

--- assets/js/modal-script.js
+++ assets/js/modal-script.js
@@ -189,3 +189,20 @@ var Modal = (function() {
 }());
 
 Modal.init();
+
+
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
